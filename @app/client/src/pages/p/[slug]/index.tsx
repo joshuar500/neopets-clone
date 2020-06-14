ccef63f268590fef0827d14e9d55babe14a0e96d
joshuar500 <memonkey@gmail.com>
2020-06-13 21:25:54 -0700

    eh

--- @app/client/src/pages/p/[slug]/index.tsx
+++ @app/client/src/pages/p/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import { SharedLayout, usePetLoading, usePetSlug } from "@app/components";
 import { PetPage_UserPetsFragment, usePetPageQuery } from "@app/graphql";
 import { Col, Row } from "antd";
+import { DateTime } from "luxon";
 import { NextPage } from "next";
 import React, { FC } from "react";
 
@@ -26,15 +27,30 @@ interface PetPageInnerProps {
   pet: PetPage_UserPetsFragment;
 }
 
-const PetPageInner: FC<PetPageInnerProps> = () => {
-  // const { pet } = props;
+const PetPageInner: FC<PetPageInnerProps> = (props) => {
+  const { pet } = props;
 
   return (
-    <Row>
-      <Col>
+    <>
+      <Row>
+        <Col span={4}>
+          <div>Level: {pet.level}</div>
+          <div>Experience: {pet.experience}</div>
+          <div>
+            Last Fed:{" "}
+            {DateTime.fromISO(pet.lastFed).toLocaleString(
+              DateTime.DATETIME_SHORT
+            )}
+          </div>
+        </Col>
+        <Col span={20}>
+          <progress className="nes-progress" value="90" max="100"></progress>
+        </Col>
+      </Row>
+      <Row>
         <img src="/images/kawaii_pixel_pet.png" />
-      </Col>
-    </Row>
+      </Row>
+    </>
   );
 };
 
